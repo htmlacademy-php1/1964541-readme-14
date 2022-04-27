77220471121c5538bbcebf6289460eb0be1550f2
g123udini <g123udini@gmail.com>
2022-04-27 13:59:06 +0300

    Делаю скрипт progile.php

--- profile.php
+++ profile.php
@@ -4,6 +4,18 @@ require_once 'helpers.php';
 require_once 'data.php';
 require_once 'session.php';
 
+
+$sql = 'SELECT title, text, quote_auth, img, video, title, text, quote_auth, img, video, link, views, user_id, p.dt_add, login, avatar FROM posts p' .
+    ' JOIN users u ON u.id = p.user_id' .
+    ' WHERE u.id = ?';
+$stmt = mysqli_prepare($connection, $sql);
+mysqli_stmt_bind_param($stmt, 'i', $user['user_id']);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+$posts = mysqli_fetch_all($result);
+var_dump($posts);
+
+
 $page_content = include_template('profile_templates/profile-window.php', ['user' => $user]);
 $layout_content = include_template('layout.php', [
     'content' => $page_content,
--- session.php
+++ session.php
@@ -2,6 +2,7 @@
 session_start();
 if (empty($_SESSION['user'])) {
     header('Location: index.php');
+    exit;
 } else {
     foreach ($_SESSION as $key => $value) {
         $user[$key] = $value;
