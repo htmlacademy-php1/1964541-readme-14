9757491a9f7e1567fc9300ed0abe667f2cab3fbd
g123udini <g123udini@gmail.com>
2022-04-27 10:40:44 +0300

    g

--- search.php
+++ search.php
@@ -4,7 +4,8 @@ require_once 'functions.php';
 require_once 'data.php';
 require_once 'session.php';
 
-
+$request = filter_input(INPUT_GET, 'search');
+var_dump($request);
 
 
 
@@ -15,5 +16,6 @@ $page_content = include_template('search_templates/search-result.php');
 $layout_content = include_template('layout.php', [
     'content' => $page_content,
     'title' => 'readme: блог, каким он должен быть',
-    'user_name' => $user_name]);
+    'user_name' => $user_name,
+    'request' => $request]);
 print($layout_content);
