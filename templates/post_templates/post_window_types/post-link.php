7cb98e6e7ab627b090914865a90faf52cee4b623
g123udini <g123udini@gmail.com>
2022-05-26 15:26:52 +0300

    Убрал дабл http

--- templates/post_templates/post_window_types/post-link.php
+++ templates/post_templates/post_window_types/post-link.php
@@ -1,6 +1,6 @@
 <div class="post__main">
     <div class="post-link__wrapper">
-        <a class="post-link__external" href="http://<?= htmlspecialchars($post['link']) ?>" title="Перейти по ссылке">
+        <a class="post-link__external" href="<?= htmlspecialchars($post['link']) ?>" title="Перейти по ссылке">
             <div class="post-link__info-wrapper">
                 <div class="post-link__icon-wrapper">
                     <img src="https://www.google.com/s2/favicons?domain=<?= htmlspecialchars($post['link']) ?>"
