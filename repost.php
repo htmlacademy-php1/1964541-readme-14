d6e107c8d3e2a5ba82ce7fa7f3fea910c0ac1e9c
g123udini <g123udini@gmail.com>
2022-05-26 15:33:25 +0300

    Нельзя сделать свой репост

--- repost.php
+++ repost.php
@@ -15,24 +15,28 @@ mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
 if ($result) {
     $post = mysqli_fetch_assoc($result);
-    $post['original_id'] = $post['id'];
-    $post['user_id'] = $user['user_id'];
+    if ($user['user_id'] !== $post['user_id']) {
+        $post['original_id'] = $post['id'];
+        $post['user_id'] = $user['user_id'];
 
-    $sql = 'INSERT INTO posts (title, text, quote_auth, img, video, link, content_type_id, user_id, original_id)
+        $sql = 'INSERT INTO posts (title, text, quote_auth, img, video, link, content_type_id, user_id, original_id)
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
-    $stmt = db_get_prepare_stmt($connection, $sql, [
-        $post['title'],
-        $post['text'],
-        $post['quote_auth'],
-        $post['img'],
-        $post['video'],
-        $post['link'],
-        $post['content_type_id'],
-        $post['user_id'],
-        $post['original_id']
-    ]);
-    $result = mysqli_stmt_execute($stmt);
-    $repost_id = mysqli_insert_id($connection);
-    header('Location: post.php?id=' . $repost_id);
+        $stmt = db_get_prepare_stmt($connection, $sql, [
+            $post['title'],
+            $post['text'],
+            $post['quote_auth'],
+            $post['img'],
+            $post['video'],
+            $post['link'],
+            $post['content_type_id'],
+            $post['user_id'],
+            $post['original_id']
+        ]);
+        $result = mysqli_stmt_execute($stmt);
+        $repost_id = mysqli_insert_id($connection);
+        header('Location: post.php?id=' . $repost_id);
+        exit;
+    }
+    header('Location: error.php?code=501');
     exit;
 }
